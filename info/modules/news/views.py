5fcde0798bd7d15513f8ef170e0539b96c0586c6
hoo <hoo@qq.com>
2019-02-11 21:52:24 +0800

    项目目录搭建，加载favicon图标，集成项目日志

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import session,render_template
+from flask import session,render_template,current_app
 
 from . import news_blue
 
@@ -6,3 +6,8 @@ from . import news_blue
 def index():
     session['itcast']='2019'
     return render_template('news/index.html')
+
+
+@news_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
