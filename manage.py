abab7580cf313e16d78cdc73d10c30815c435c51
hoo <hoo@qq.com>
2019-02-11 20:00:06 +0800

    项目目录搭建，实现配置数据库，迁移框架，脚本管理器

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 from flask import Flask,session
 
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_script import Manager
+
+from flask_migrate import Migrate,MigrateCommand
+
 from flask_session import Session
 
 from redis import StrictRedis
@@ -9,8 +15,13 @@ app.config['SESSION_TYPE']='redis'
 app.config['SESSION_REDIS']=StrictRedis(host='127.0.0.1',port=6379)
 app.config['SESSION_USE_SINGER']=True
 app.config['PERMANENT_SESSION_LIFETIME']=86400
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:mysql@localhost/info22'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 Session(app)
+db=SQLAlchemy(app)
+manage=Manager(app)
+Migrate(app,db)
+manage.add_command('db',MigrateCommand)
 
 
 @app.route('/')
@@ -19,4 +30,4 @@ def index():
     return 'hello world'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manage.run()
